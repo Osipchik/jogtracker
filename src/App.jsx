cb13f77cb5e73526968ba8990af00755ab1b0e65
Tim <WyTrapT@outlook.com>
2021-01-30 12:50:37 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+    return (
+        <div className="container">
+            App
+            
+        </div>
+    )
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        'presets': ['@babel/preset-env', '@babel/preset-react'],
+                        'plugins': [ 'react-hot-loader/babel', '@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                },
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        watchContentBase: true,
+        compress: true,
+        hot: true,
+        overlay: true,
+        open: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            scriptLoading: 'defer'
+        })
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+
+        splitChunks: {
+            chunks: 'all',
+
+            minSize: 20000,
+            minRemainingSize: 0,
+            maxSize: 50000,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    }
+};
