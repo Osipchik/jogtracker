318b7dcb02f2ef832a41a5c964c89a694e81bc16
Tim <WyTrapT@outlook.com>
2021-02-02 09:20:39 +0300

    fix: folder name ligin -> login

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -6,7 +6,7 @@ import {
     Redirect
 } from "react-router-dom";
 import NavBar from "../NavBar";
-import Login from "../../pages/Ligin";
+import Login from "../../pages/Login";
 import Info from "../../pages/Info";
 import Jogs from "../../pages/Jogs";
 import EmptyJog from "../../pages/EmptyJog/index";
